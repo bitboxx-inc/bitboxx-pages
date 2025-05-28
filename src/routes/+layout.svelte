ca74c8780a4c53520dcbd6388bd76ae53e2e6feb
digiot <digiot.abc@gmail.com>
2025-05-29 04:32:25 +0900

    redesign

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,10 @@
 <script>
     import "../app.css";
+
+    const links = [
+        {text: "コンセプト", href: "/#concept"},
+        {text: "サービスプラン", href: "/#plans"}
+    ]
 </script>
 
 <svelte:head>
@@ -7,23 +12,57 @@
 </svelte:head>
 
 
-<div class="container mx-auto max-w-1280px">
-    <header class="my-8">
-        <div class="flex">
-            <div>
-                <img src="/images/logo.svg" alt="bitboxx logo" width="290" height="51" onclick="location.href='/'"/>
+<div class="container mx-auto max-w-[1280px]">
+    <!-- ✅ デスクトップ版：md以上で表示 -->
+    <div class="hidden md:block">
+        <header class="my-8 ml-8">
+            <div class="flex">
+                <div>
+                    <img class="max-w-[290px]" src="/images/logo.svg" alt="bitboxx logo" height="51" onclick="location.href='/'"/>
+                </div>
+                <div class="ml-auto mr-8 my-auto">
+                    {#each links as link}
+                        <a href="{link.href}" class="ml-4"><b>{link.text}</b></a>
+                    {/each}
+                    <span class="ml-4">
+                        <button class="bg-yellow hover:bg-yellow_light
+                            text-black font-bold rounded-full px-8 py-1 shadow-[0_4px_0_#000] border focus:ring-2 focus:ring-yellow-500">
+                            問い合わせ
+                        </button>
+                    </span>
+                </div>
             </div>
-            <div class="ml-auto">
-                コンセプト
-                サービスプラン
-                お問い合わせ
+        </header>
+    </div>
+
+    <!-- ✅ モバイル版：md未満で表示 -->
+    <div class="block md:hidden">
+        <header class="mx-auto my-2">
+            <div class="flex justify-center my-6">
+                <img class="max-w-[290px]" src="/images/logo.svg" alt="bitboxx logo" height="51" onclick="location.href='/'"/>
             </div>
-        </div>
-    </header>
-    <slot />
+            <div class="flex justify-center mb-8">
+                <div class="my-auto">
+                    {#each links as link}
+                        <a href="{link.href}" class="ml-4"><b>{link.text}</b></a>
+                    {/each}
+                    <span class="ml-4">
+                        <button class="bg-yellow hover:bg-yellow_light
+                            text-black font-bold rounded-full px-8 py-1 shadow-[0_4px_0_#000] border focus:ring-2 focus:ring-yellow-500">
+                            問い合わせ
+                        </button>
+                    </span>
+                </div>
+            </div>
+        </header>
+    </div>
+
+    <slot/>
+
     <footer>
         <div class="flex justify-center">
             2024 &copy; bitboxx inc.
         </div>
     </footer>
 </div>
+
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,14 +7,14 @@
 </svelte:head>
 
 <div class="flex">
-    <div class="flex flex-col md:flex-row bg-gray">
+    <div class="flex flex-col md:flex-row border-to bg-gray min-h-[393px]">
         <!-- 左側: 画像 -->
-        <div class="flex-1 flex items-center justify-center bg-gray">
-            <img src="/images/top1.svg" alt="logo" class="max-w-full h-auto"/>
+        <div class="flex items-center justify-center max-w-[786px] ">
+            <img src="/images/top1.svg" alt="logo" class="w-full h-auto"/>
         </div>
         <!-- 右側: テキスト -->
-        <div class="flex-1 flex items-center justify-center bg-gray">
-            <div class="w-[395px] h-[155px] flex flex-col justify-center items-center">
+        <div class="flex-1 flex items-center justify-center ">
+            <div class="flex-1 md:w-[494px] h-[155px] flex flex-col justify-center items-center">
                 <div class="font-bold text-4xl font-noto mb-1 text-center">ウェブサイト</div>
                 <div class="font-bold text-4xl font-noto mb-2 text-center">すべて、おまかせ</div>
                 <div class="text-lg font-noto text-center">プロがつくる、まもる、そだてる。</div>
@@ -26,10 +26,36 @@
 <!-- コンセプト -->
 <div class="bg-light_yellow">
     <div class="flex justify-center items-center pt-2">
-        <div class="text-4xl my-12 font-noto"><b>コンセプト</b></div>
+        <div id="concept" class="text-4xl my-12 font-noto"><b>コンセプト</b></div>
+    </div>
+    <div class="flex flex-col md:flex-row border-to">
+        <!-- 左側: 画像 -->
+        <div class="flex-1 items-center justify-center md:min-w-[480px] md:max-w-[50%]">
+            <div class="md:min-h-[364px] md:mx-16 mx-8 mb-16 rounded-lg bg-white">
+                <img class="mx-auto pt-6" src="/images/top2_light.svg" width="64" height="64" alt="logo"/>
+                <div class="font-bold text-xl font-noto text-center mt-4">ウェブサイト制作、<br/>もっとラクをしよう</div>
+                <div class="font-noto text-center mt-8 md:mx-20 mx-4 text-base pb-8">
+                    bitboxx PAGESは、「月額10万円〜」で、ウェブサイトの企画・構成・デザインから、保守・運用・セキュリティまでワンストップで対応。プロがすべて対応します。
+                </div>
+            </div>
+        </div>
+        <!-- 右側: テキスト -->
+        <div class="flex-1 items-center justify-center md:max-w-[50%]">
+            <div class="md:mx-4 mx-8 mb-16">
+                <div class="font-bold md:text-2xl font-noto text-center mt-4">＼　ご希望に合わせて、まるごと対応！　／</div>
+                <img class="mx-auto pt-6" src="/images/top2_pc.svg"alt="logo"/>
+            </div>
+        </div>
     </div>
 </div>
 
+<!-- サービスプラン -->
+<div class="bg-gray">
+    <div class="flex justify-center items-center pt-2">
+        <div id="plans" class="text-4xl my-12 font-noto"><b>サービスプラン</b></div>
+    </div>
+    準備中
+</div>
 <!--<div class="h-dvh">-->
 
 <!--    <Products/>-->
